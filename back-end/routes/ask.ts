92e8345d7b9777704f8f1d14636418227440d13d
Edgar <edgarmatoto34@gmail.com>
2024-06-18 00:58:41 +0700

    Add ExpressJS, add /api/ask route, add typescript

---
+++ back-end/routes/ask.ts
@@ -0,0 +1,31 @@
+import express, {NextFunction, Router, Request, Response} from "express";
+import {
+    EnhancedGenerateContentResponse,
+    GenerateContentResult,
+    GenerativeModel,
+    GoogleGenerativeAI
+} from "@google/generative-ai";
+
+require("dotenv").config();
+
+const router: Router = express.Router();
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(process.env.GEMINI_KEY || "");
+const model: GenerativeModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+async function run(question: string): Promise<any> {
+    const result: GenerateContentResult = await model.generateContent(question);
+    const response: EnhancedGenerateContentResponse = await result.response;
+
+    return response.text()
+}
+
+/* GET users listing. */
+router.get('/', async function (req: Request, res: Response, next: NextFunction): Promise<any> {
+    const answer = await run("berikan nasehat singkat untuk orang dengan gangguan mental");
+
+    res.send(answer);
+});
+
+export default router;
